5033eb432677dea4376b54465b841da883b1ca74
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-14 02:57:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  while i < katz_deli.length - 1
+    new_line = []
+    i = 0
+    new_line.push([i+1] + ". " + katz_deli[i])
+  end
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{new_line.join(", ")}"
+  end
+end
