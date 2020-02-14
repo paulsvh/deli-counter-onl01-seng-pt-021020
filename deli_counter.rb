bbb9e141de404382f584347a2f61d099fda4f684
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-14 03:12:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,8 @@ end
 def now_serving(katz_deli)
   line_number = katz_deli.length
   if line_number > 0
-    puts "Currently serving #{katz_deli[line_number]}."
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   else
     puts "There is nobody waiting to be served!"
   end
